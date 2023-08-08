5c5ce059e071a187a6e2799d8f18a60980dd3f5c
Egor Sergeevich <122815068+EgorSereevich@users.noreply.github.com>
2023-08-08 18:51:45 +0300

    08_02_commit

--- src/components/Searchbar/Searchbar.jsx
+++ src/components/Searchbar/Searchbar.jsx
@@ -3,42 +3,61 @@ import axios from 'axios';
 export class Searchbar extends Component {
   state = {
     search: '',
-    images: {},
+    statusSerch: true,
+    loadImages: [],
+    page: 1,
+    perpage: 12,
   };
   async onSearch() {
     const KEY_PIX = '37771567-c63b0fa1e82728e8a21c21132';
-    const axiosSerch = await axios.get(
-      `https://pixabay.com/api/?q=${this.state.search}&page=1&key=${KEY_PIX}&image_type=photo&orientation=horizontal&per_page=12`
+    const fetchImages = await axios.get(
+      `https://pixabay.com/api/?q=${this.state.search}&page=${this.state.page}&key=${KEY_PIX}&image_type=photo&orientation=horizontal&per_page=${this.state.perpage}`
     );
-    this.setState({ images: axiosSerch });
-    console.log(this.state.images);
+    this.setState(prevState => ({
+      loadImages:
+        this.state.page === 1
+          ? fetchImages.data.hits
+          : [fetchImages.data.hits, ...prevState.loadImages],
+    }));
   }
+  headleInput = e => {
+    const searchImages = e.target.value;
+    this.setState({ search: searchImages });
+    console.log(this.state.search);
+  };
   onSubmit = evt => {
     evt.preventDefault();
-    const searchImages = evt.currentTarget.search.value;
-    this.setState({ search: searchImages });
     this.onSearch();
   };
+  loadMoreImages = () => {
+    this.setState(prevState => ({ page: prevState.page + 1 }));
+    this.onSearch();
+  };
+
   render() {
+    const { loadImages } = this.state;
     return (
       <div>
         <div>
           <form action="" onSubmit={this.onSubmit}>
-            <input type="text" name="search" />
+            <input type="text" name="search" onChange={this.headleInput} />
             <button type="submit">Search</button>
           </form>
         </div>
-        {/* <div>
+        <div>
           <ul>
-            {axiosSerch.map(img => {
+            {loadImages.map(img => {
               return (
-                // <li>
-                //   <img src="" alt="" />
-                // </li>
+                <li key={img.id}>
+                  <img src={img.previewURL} alt="" />
+                </li>
               );
             })}
           </ul>
-        </div> */}
+        </div>
+        <button type="button" onClick={this.loadMoreImages}>
+          Load More Images
+        </button>
       </div>
     );
   }
